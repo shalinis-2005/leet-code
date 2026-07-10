a958736fb93848a4c9937be63466d299d2e0ee73
shalinis-2005 <shalinis.cse2023@dce.edu.in>
2026-07-10 18:32:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (75.76%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,44 +1,25 @@
 class MyStack {
-    Queue<Integer> q1=new ArrayDeque<>();
+    Stack<Integer> s1=new Stack<>();
     public MyStack() {
         
     }
     
     public void push(int x) {
-     q1.offer(x);  
+     s1.push(x);  
     }
     
     public int pop() {
-        int size=q1.size()-1;
-        while(size!=0)
-        {
-            int val=q1.peek();
-            q1.poll();
-            q1.offer(val);
-            size--;
-        }
-        var it=q1.peek();
-        q1.poll();
-        return it;
-    }
-    
-    public int top() {
-    int size=q1.size()-1;
-        while(size!=0)
-        {
-            int val=q1.peek();
-            q1.poll();
-            q1.offer(val);
-            size--;
-        }
-        var it=q1.peek();
-        q1.poll();
-        q1.offer(it);
+        var it=s1.peek();
+        s1.pop();
         return it;   
     }
     
+    public int top() {
+    return s1.peek();    
+    }
+    
     public boolean empty() {
-    if(q1.isEmpty()==true)
+    if(s1.isEmpty()==true)
     { 
         return true;
     } 
